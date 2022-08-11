670d1212e69719e941eb7f2aefcc063df5e736f6
pepe_fabra_valverde <jofaval@outlook.com>
2022-08-11 22:08:27 +0200

    fix(Certificates): Fix minor inconsistencies

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -13,28 +13,49 @@ type CertificateType = {
   organization?: string | null;
 };
 
-const Certificate: React.FC<CertificateType> = () => (
+const Certificate: React.FC<CertificateType> = ({ title, description }) => (
   <div className="certificate__container">
-    <div className="certificate"></div>
+    <div className="certificate card">
+      <p
+        className="certificate__title"
+        style={{
+          fontWeight: "bold",
+          fontSize: "1.25rem",
+          textAlign: "center",
+        }}
+      >
+        {title}
+      </p>
+      <br />
+      {description && (
+        <span className="certificate__description">{description}</span>
+      )}
+    </div>
   </div>
 );
 
-const renderCertificate: (certificate: CertificateType) => JSX.Element = (
-  certificate
-) => <Certificate {...certificate} />;
+const renderCertificate: (
+  certificate: CertificateType,
+  index: number
+) => JSX.Element = (certificate, index) => (
+  <Certificate {...certificate} key={`certificate_${index}`} />
+);
 
 const Certificates: React.FC<{
   certificates?: CertificateType[];
 }> = ({ certificates = certificatesData as CertificateType[] }) => {
   const preparedCertificates = useMemo(() => {
-    return certificates;
+    let newCertificates = [...certificates];
+    newCertificates = newCertificates.filter((certificate, _) => true);
+    return newCertificates;
   }, [certificates]);
 
   return (
     <div className="certificates__container">
       <div className="certificates__title">Certificates</div>
       <div className="certificates">
-        {preparedCertificates && preparedCertificates.map(renderCertificate)}
+        {Array.isArray(preparedCertificates) &&
+          preparedCertificates?.map(renderCertificate)}
       </div>
     </div>
   );
